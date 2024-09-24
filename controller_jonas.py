cd3d2d8586fc55bf91fd4f654cf04481d1f87e06
Jonas Schaefer <jonas.schaefer00@gmail.com>
2024-09-24 11:28:16 +0200

    Minor changes, small improvements, data logging

--- controller_jonas.py
+++ controller_jonas.py
@@ -11,20 +11,32 @@ class controller_jonas(Controller):
         self.n_inputs = 20
         self.n_outputs = 5
 
+        hidden_weights_size = self.n_inputs * self.n_hidden
+        hidden_bias_size = self.n_hidden
+        output_weights_size = self.n_hidden * self.n_outputs
+        output_bias_size = self.n_outputs
+
+        expected_size = (hidden_weights_size + hidden_bias_size +
+                         output_weights_size + output_bias_size)
+        assert len(weights) == expected_size, f"Expected weights array of size {expected_size}, but got {len(weights)}"
+
+        self.hidden_layer_weights = np.array(weights[:hidden_weights_size]).reshape((self.n_inputs, self.n_hidden))
+        self.hidden_layer_bias = np.array(weights[hidden_weights_size:hidden_weights_size + hidden_bias_size])
+        self.output_layer_weights = np.array(weights[hidden_weights_size + hidden_bias_size:
+                                                     hidden_weights_size + hidden_bias_size + output_weights_size]).reshape((self.n_hidden, self.n_outputs))
+        self.output_layer_bias = np.array(weights[-output_bias_size:])
+
     def control(self, inputs, controller):
         assert self.weights is not None, "Weights have not been set."
 
         inputs = np.array(inputs)
         assert len(inputs) == self.n_inputs, f"Expected {self.n_inputs} inputs, but got {len(inputs)}. This shouldn't happen."
 
-        expected_size = self.n_inputs * self.n_inputs + self.n_inputs * self.n_outputs
-        assert len(self.weights) == expected_size, f"Expected weights array of size {expected_size}, but got {len(self.weights)}"
+        # Forward pass through the hidden layer
+        hidden_layer_output = np.tanh(np.dot(inputs, self.hidden_layer_weights) + self.hidden_layer_bias)
 
-        hidden_layer_weights = np.array(self.weights[:self.n_inputs * self.n_inputs]).reshape((self.n_inputs, self.n_inputs))
-        output_layer_weights = np.array(self.weights[self.n_inputs * self.n_inputs:]).reshape((self.n_inputs, self.n_outputs))
-
-        hidden_layer_output = np.tanh(np.dot(inputs, hidden_layer_weights))
-        output = np.tanh(np.dot(hidden_layer_output, output_layer_weights))
+        # Forward pass through the output layer
+        output = np.tanh(np.dot(hidden_layer_output, self.output_layer_weights) + self.output_layer_bias)
 
         # Convert the output to control signals
         left = 1 if output[0] > 0 else 0
